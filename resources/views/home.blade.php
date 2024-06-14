284f941636e126b38dc10abde6c716786d5f50b9
Putra 21.11.4373 <101878644+Putrabudi@users.noreply.github.com>
2024-06-14 10:07:06 +0700

    Add files via upload

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,14 @@
-@extends('layouts.app')
+@extends('layouts.sbadmin2')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="card">
+        <div class="card-header">{{ __('Dashboard') }}</div>
+        <div class="card-body">
+            <div>
+                <a href="/administrasi/create" class="btn btn-primary m-1">Buat Administrasi</a>
+                <a href="/pasien/create" class="btn btn-primary m-1">Tambah Pasien</a>
+                <a href="/dokter/create" class="btn btn-primary m-1">Tambah Dokter</a>
             </div>
         </div>
     </div>
-</div>
 @endsection
